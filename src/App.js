b7d1ee7de2b8d428bb7227443fa9f8db1d233245
Dinesh <dinesh.melapundi@gmail.com>
2021-06-20 16:05:15 +0530

    movie list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+import List from './containers/List';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <nav className='navbar sticky-top navbar-light bg-dark'>
+        <h1 className='navbar-brand text-light'>MovieList</h1>
+      </nav>
+
+      <List />
     </div>
   );
-}
+};
 
 export default App;
